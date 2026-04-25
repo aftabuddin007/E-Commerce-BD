c060c79abdfe57f45f218e230531d6d958dc1aba
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-04-25 23:11:01 +0600

    navber with raw css

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import './Navbar.css'
+import React, { useState } from 'react';
+import './Navbar.css';
 import logo from '../assets/logo.webp';
 import { FaCartPlus } from "react-icons/fa";
 const Navbar = () => {
+    const [menu,setMenu] = useState("shop");
+    
     return (
         <div className='navbar'>
             <div className="nav-logo">
-                <img src={logo} alt="" />
+                <img src={logo} height={60} width={60} alt="" />
                 <p className="">E-CommerceBD</p>
             </div>
             <ul className="nav-menu">
-                <li className="">Shop</li>
-                <li className="">Men</li>
-                <li className="">Women</li>
-                <li className="">Kids</li>
+                <li onClick={()=>{setMenu("shop")}} className="">Shop{menu === "shop"?<hr />:<></>} </li>
+                <li onClick={()=>{setMenu("mens")}} className="">Men {menu === "mens"?<hr />:<></>}</li>
+                <li onClick={()=>{setMenu("womens")}} className="">Women {menu === "womens"?<hr />:<></>}</li>
+                <li onClick={()=>{setMenu("kids")}} className="">Kids {menu === "kids"?<hr />:<></>}</li>
             </ul>
             <div className="nav-login-cart">
                 <p><FaCartPlus /></p>
+                <div className="nav-cart-count">0</div>
                 <button>Login</button>
             </div>
         </div>
