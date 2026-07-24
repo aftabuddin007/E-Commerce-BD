1b018e8d9caf69f2014a5271a3f7801dc11253a9
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-07-24 22:30:36 +0600

    add footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import "./Footer.css";
 
-const Footer = () => {
-    return (
-        <div>
-            
+function Footer() {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-brand">
+          <h2>MyWebsite</h2>
+          <p>Building modern web experiences.</p>
         </div>
-    );
-};
+
+        <div className="footer-links">
+          <h3>Quick Links</h3>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/services">Services</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </div>
+
+        <div className="footer-social">
+          <h3>Follow Us</h3>
+          <div className="social-icons">
+            <a href="https://facebook.com" target="_blank" rel="noreferrer">
+              Facebook
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noreferrer">
+              Twitter
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noreferrer">
+              Instagram
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer-bottom">
+        <p>&copy; {year} MyWebsite. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
 
 export default Footer;
